ce41edda12aad4d98b9123f38c7ebfb25e7bd054
Erica  Cohen <ericacohen@Ericas-Air.lan>
2018-01-06 22:01:50 -0800

    wantToRestart finction in progress

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,7 +12,7 @@ var connection = mysql.createConnection({
     user: "root",
 
     // Your password
-    password: "",
+    password: "Buzzer123#",
     database: "bamazonDB"
 });
 
@@ -22,6 +22,7 @@ connection.connect(function (err) {
     displayItems();
 });
 
+//function displays all of the items that are in stock to the user
 function displayItems() {
     connection.query("SELECT * FROM products", function (err, res) {
         for (var i = 0; i < res.length; i++) {
@@ -32,6 +33,7 @@ function displayItems() {
     });
 }
 
+//function allows user to select the item and quantity they want 
 function whatItemDoesUserWant() {
     inquirer
         .prompt([{
@@ -48,48 +50,68 @@ function whatItemDoesUserWant() {
             }
         ])
 
-        .then(function(input) {
+        .then(function (input) {
+            //variable that takes in the ID of the item the user wishes to purchase
             var itemIDRequestedByUser = input.ID;
+            //variable that takes in the quantity of the item the user wants to purchase
             var userRequestedQuantity = input.Quantity;
+            //setting parameters for the purchase
             makePurchaseIfEnoughInStock(itemIDRequestedByUser, userRequestedQuantity);
         })
-        // .then(function (input) {
-        //     connection.query("SELECT * FROM products WHERE id = ?", [input.item_id], function (err, res) {
-        //         if (input.item_quantity > res[0].stock_quantity) {
-        //             console.log("Insufficent Quantity!");
-        //             displayItems();
-        //         } else {
-        //             amoutUserOwes = res[0].price * answer.itemQuantity;
-        //             console.log("Your purchase is complete");
-        //         }
-        //     })
-        // })
 }
 
+//this function checks if there are enough of the item in stock and if there is allows user to select item and how many they want
 function makePurchaseIfEnoughInStock(ID, Quantity) {
-    connection.query('SELECT * FROM products WHERE item_id = ' + ID, function(err, res){
+    connection.query('SELECT * FROM products WHERE item_id = ' + ID, function (err, res) {
+        //checks to see if the Quantity the user wants is less then or equal to the amount of the item that is in stock
         if (Quantity <= res[0].stock_quantity) {
+            //if there is enough of the item then user is notified that their purchase went through
             console.log("Item has been purchased!");
+            //caluclating the users bill
             totalCost = res[0].price * Quantity;
+            //telling the user their bill
             console.log("Order total cost: $" + totalCost);
             console.log("-----------------------------------");
-            displayItems();
+            //calling this function from below which will give user option to continue shopping or complete their order
+            wantToRestart();
             // connection.query('UPDATE products SET stock_quantity = stock_quantity - ' + Quantity + 'WHERE item_id = ' + ID);
             connection.query(
-                "UPDATE products SET ? WHERE ?",
-                [
-                    {
-                        stock_quantity: Quantity
+                "UPDATE products SET ? WHERE ?", [{
+                        stock_quantity: stock_quantity - Quantity
                     },
                     {
                         item_id: ID
                     }
                 ]
             )
+        //this will run if the user requests to purchase more of an item then bamazon has in stock
         } else {
             console.log("We dont have enough of this item.");
             console.log("-----------------------------------");
-            displayItems();
+            //calling this function from below which will give user option to continue shopping or complete their order
+            wantToRestart();
         }
     })
+}
+
+//this function allows the user to select if they want to purchase another item of complete their order
+function wantToRestart() {
+    inquirer
+        .prompt({
+            name: "yesOrNo",
+            type: "rawlist",
+            message: "Would you like to buy something else?",
+            choices: ["Yes", "No"]
+        })
+        .then(function (answer) {
+            // based on their answer, either call the yes or the no functions
+            //if the user wants to continue shopping
+            if (answer.yesOrNo === "Yes") {
+                //display items available 
+                displayItems();
+            } else {
+                //end the purchase
+                console.log("Have a nice day")
+            }
+        });
 }
